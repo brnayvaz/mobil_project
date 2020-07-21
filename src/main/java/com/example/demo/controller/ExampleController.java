d3b1ad25697fa4533d50831395aa842511bd2ae8
Burhan.Ayvazoglu <Burhan.Ayvazoglu@burhannb.ITPORTER.COM>
2020-07-21 07:46:00 +0300

    değişklik2

--- src/main/java/com/example/demo/controller/ExampleController.java
+++ src/main/java/com/example/demo/controller/ExampleController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import javax.xml.ws.Response;
+
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
